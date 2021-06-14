1bb7c2336056a02db99cdf482492cf07f8019bcb
Antonio Gomes Castro Junior <AN20164347@wipro.com>
2021-06-14 16:25:46 -0300

    Basket microservice developed

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,39 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache reditCache;
+        public BasketRepository(IDistributedCache reditCache)
+        {
+            this.reditCache = reditCache;
+        }
+        public async Task DeleteBasket(string userName)
+        {
+            await reditCache.RefreshAsync(userName);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await reditCache.GetStringAsync(userName);
+            if (String.IsNullOrEmpty(basket))
+                return null;
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await reditCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return await GetBasket(basket.UserName);
+        }
+    }
+}
