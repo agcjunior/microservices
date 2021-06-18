ab241ca66e26ecace8f8738e9aaceff769df3ac5
Antonio Gomes Castro Junior <AN20164347@wipro.com>
2021-06-18 16:01:15 -0300

    Ordering layers created and first Query implemented

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrderListQuery, List<OrdersVm>>
+    {
+        private readonly IOrderRepository repository;
+        private readonly IMapper mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository repository, IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+        public async Task<List<OrdersVm>> Handle(GetOrderListQuery request, CancellationToken cancellationToken)
+        {
+            var orderList = await repository.GetOrdersByUsername(request.UserName);
+            return mapper.Map<List<OrdersVm>>(orderList);
+        }
+    }
+}
