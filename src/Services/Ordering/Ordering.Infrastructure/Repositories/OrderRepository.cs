888613158d9e207d654eaaad696a65d86850c673
Antonio Gomes Castro Junior <AN20164347@wipro.com>
2021-06-30 13:31:38 -0300

    Infrastructure implemented

---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbcontext) : base(dbcontext)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUsername(string username)
+        {
+            var orderList = await orderContext.Orders
+                                    .Where(o => o.UserName == username)
+                                    .ToListAsync();
+            return orderList;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/RepositoryBase.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Common;
+using Ordering.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class RepositoryBase<T> : IAsyncRepository<T> where T : EntityBase
+    {
+        protected readonly OrderContext orderContext;
+
+        public RepositoryBase(OrderContext orderContext)
+        {
+            this.orderContext = orderContext;
+        }        
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await orderContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await orderContext.Set<T>().Where(predicate).ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeString = null, bool disableTracking = true)
+        {
+            IQueryable<T> query = orderContext.Set<T>();
+            if (disableTracking)
+                query = query.AsNoTracking();
+
+            if (!string.IsNullOrWhiteSpace(includeString))
+                query = query.Include(includeString);
+
+            if (predicate != null)
+                query = query.Where(predicate);
+
+            if (orderBy != null)
+                return await orderBy(query).ToListAsync();
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<Expression<Func<T, object>>> includes = null, bool disableTracking = true)
+        {
+            IQueryable<T> query = orderContext.Set<T>();
+            if (disableTracking)
+                query = query.AsNoTracking();
+
+            if (includes != null)
+                query = includes.Aggregate(query, (current, include) => current.Include(include));
+
+            if (predicate != null)
+                query = query.Where(predicate);
+
+            if (orderBy != null)
+                return await orderBy(query).ToListAsync();
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await orderContext.Set<T>().FindAsync(id);
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            orderContext.Set<T>().Add(entity);
+            await orderContext.SaveChangesAsync();
+            return entity;
+        }
+        public async Task DeleteAsync(T entity)
+        {
+            orderContext.Set<T>().Remove(entity);
+            await orderContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            orderContext.Entry(entity).State = EntityState.Modified;
+            await orderContext.SaveChangesAsync();
+        }
+    }
+}
